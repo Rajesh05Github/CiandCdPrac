e8faf37e2e14593498814c87f382e2d6c1d84b40
Rajesh038-git <rajeshkanojia903@gmail.com>
2025-05-11 00:42:09 +0530

    change to handle dynacmic changes

--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,15 @@ dbConnection()
 
 
 const corsOptions = {
-  origin: ["http://localhost:5173","http://localhost:5004","http://cicdfrontend.rajeshit.xyz"],
-  methods: ["GET", "POST"],
+  origin: [
+    "http://localhost:5173",               
+    "http://cicdfrontend.rajeshit.xyz"    
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
 };
+
 
 app.use(cors(corsOptions))
 
